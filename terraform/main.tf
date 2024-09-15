54b09a9c5c01ff3bfe4e112569ed90cee2b47d47
Shinkeishi <ashleyytanjx@gmail.com>
2024-09-15 18:26:30 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,71 +14,142 @@ variable "number_of_users" {
 locals {
   users = [for u in range(1, var.number_of_users + 1) : {
     user_id  = u
-    username = format("student%s", u)
+    username = format("student-%s", u)
   }]
 
   user_map = { for user in local.users : user.username => user }
 }
 
-# Create IAM users
+# create IAM users
 resource "aws_iam_user" "students" {
   for_each = local.user_map
   name     = each.value.username
 }
 
-# Create access keys for the IAM users
+# create access keys for the IAM users
 resource "aws_iam_access_key" "student_keys" {
   for_each = aws_iam_user.students
   user     = aws_iam_user.students[each.key].name
 }
 
-# Create a custom read-write policy for S3 and EC2
-resource "aws_iam_policy" "custom_rw_policy" {
-  name = "CustomReadWritePolicy"
+resource "aws_iam_user_login_profile" "student_logins" {
+  for_each                = aws_iam_user.students
+  user                    = aws_iam_user.students[each.key].name
+  password_reset_required = false
+}
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket",
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:DeleteObject"
-      ],
-      "Resource": "arn:aws:s3:::*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:DescribeInstances",
-        "ec2:RunInstances",
-        "ec2:StopInstances",
-        "ec2:TerminateInstances"
-      ],
-      "Resource": "*"
+# EC2: create only one fixed size
+resource "aws_iam_user_policy" "ec2_policy" {
+  for_each = aws_iam_user.students
+  name     = "workshop-student-${each.key}-ec2"
+  user     = aws_iam_user.students[each.key].name
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:RunInstances",
+          "ec2:TerminateInstances",
+          "ec2:DescribeInstances"
+        ],
+        "Resource" : "*",
+        "Condition" : {
+          "StringEquals" : {
+            "ec2:InstanceType" : "t2.micro"
+          },
+          "NumericLessThanEquals" : {
+            "ec2:InstanceCount" : "1"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# special naming for aurora serverless instance for each student
+resource "aws_iam_user_policy" "aurora_serverless_policy" {
+  for_each = aws_iam_user.students
+  name     = "workshop-student-${each.key}-aurora-serverless"
+  user     = aws_iam_user.students[each.key].name
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "rds:CreateDBCluster",
+          "rds:CreateDBInstance",
+          "rds:DeleteDBCluster",
+          "rds:DeleteDBInstance",
+          "rds:DescribeDBClusters",
+          "rds:DescribeDBInstances"
+        ],
+        "Resource" : "*",
+        "Condition" : {
+          "StringLike" : {
+            "rds:DatabaseName" : "smuworkshop-${each.key}"
+          },
+          "StringEquals" : {
+            "rds:Engine" : "aurora",
+            "rds:DBClusterClass" : "db.serverless"
+          },
+          "NumericLessThanEquals" : {
+            "ec2:InstanceCount" : "1"
+          }
+        }
+      }
+    ]
     }
-  ]
-}
-EOF
+  )
 }
 
-# Attach the custom policy to each user
-resource "aws_iam_user_policy_attachment" "student_custom_policy" {
-  for_each   = aws_iam_user.students
-  user       = aws_iam_user.students[each.key].name
-  policy_arn = aws_iam_policy.custom_rw_policy.arn
+# special s3 naming policy for each student
+resource "aws_iam_user_policy" "workshop_student_s3" {
+  for_each = aws_iam_user.students
+  name     = "workshop-student-${each.key}-s3"
+  user     = aws_iam_user.students[each.key].name
+
+  policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Effect : "Allow",
+        Action : "*",
+        Resource : "arn:aws:s3:::smuworkshop-${each.key}"
+      },
+      {
+        Effect : "Allow",
+        Action : ["s3:ListAllMyBuckets", ],
+        Resource : "*"
+      },
+      {
+        Effect : "Allow",
+        Action : "*",
+        Resource : "arn:aws:s3:::smuworkshop-${each.key}/*"
+      },
+      {
+        Effect : "Allow",
+        Action : [
+          "iam:ListRoles",
+        ],
+        Resource : "*"
+      },
+
+    ]
+  })
 }
 
-# Generate a CSV file with user credentials
+# generate a CSV file with user credentials
 locals {
   combined = [
     for k, user in aws_iam_user.students : {
       user   = user.name
       key    = aws_iam_access_key.student_keys[k].id
       secret = aws_iam_access_key.student_keys[k].secret
+      pass   = aws_iam_user_login_profile.student_logins[k].password
     }
   ]
 }
@@ -86,7 +157,7 @@ locals {
 resource "local_file" "credentials" {
   filename = "creds.csv"
   content  = <<-EOF
-    User,AccessKey,SecretKey
-    ${join("\n", [for item in local.combined : format("%s,%s,%s", item.user, item.key, item.secret)])}
+    User,Password,AccessKey,SecretKey
+    ${join("\n", [for item in local.combined : format("%s,%s,%s,%s", item.user, item.pass, item.key, item.secret)])}
   EOF
 }

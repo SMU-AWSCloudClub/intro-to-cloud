e097e5cb3cdf4090377adbf58dd4592e07d2a30a
Yue Zhen <smithquaz@gmail.com>
2024-10-17 19:48:05 +0800

    fix: health check endpoint

--- backend/nestjs/src/main.ts
+++ backend/nestjs/src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.setGlobalPrefix('nestjs');
+
   // Set up Swagger
   const config = new DocumentBuilder()
     .setTitle('Intro to Cloud Deployment')
